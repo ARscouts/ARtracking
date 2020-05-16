2f102c9ec7644b4f5716f45f4cf52ecc0efa4f34
Ivan <vakulko.ivan@gmail.com>
2020-05-16 21:15:01 +0200

    Added menu, and changes ingame mode

---
+++ Assets/Scripts/CreateObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+    public Transform Player;
+    public Transform Object1;
+    Vector3 center;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        center.x = Player.position.x;
+        center.y = Player.position.y;
+        center.z = Player.position.z;
+
+        Object1.position = new Vector3(center.x+2, 0, center.z+2);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
