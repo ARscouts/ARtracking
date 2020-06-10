f2a6bd5262e270e1d5cda48d0141f8858e003f12
Ivan <vakulko.ivan@gmail.com>
2020-06-10 21:10:58 +0200

    Options panel ready

---
+++ Assets/Scripts/Options/ValuesChange.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValuesChange : MonoBehaviour
+{
+    public Slider ClueCountSlider;
+    public Slider MaxSlider;
+    public Slider MinSlider;
+    public Text ClueCountInfo;
+    public Text MaxInfo;
+    public Text MinInfo;
+
+    public IntVariable ClueCount;
+    public FloatVariable MaxTrackingDistance;
+    public FloatVariable MinTrackingDistance;
+
+    void Start()
+    {
+        ClueCountSlider.value = ClueCount.Value;
+        ClueCountUpdate();
+        MaxSlider.value = MaxTrackingDistance.Value;
+        MaxUpdate();
+        MinSlider.value = MinTrackingDistance.Value;
+        MinUpdate();
+
+    }
+
+    public void ClueCountUpdate()
+    {
+        ClueCountInfo.text = ClueCountSlider.value.ToString();
+        ClueCount.Value = (int)ClueCountSlider.value;
+
+    }
+    public void MaxUpdate()
+    {
+        MaxInfo.text = MaxSlider.value.ToString();
+        MaxTrackingDistance.Value = MaxSlider.value;
+
+    }
+    public void MinUpdate()
+    {
+        MinInfo.text = MinSlider.value.ToString();
+        MinTrackingDistance.Value = MinSlider.value;
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
