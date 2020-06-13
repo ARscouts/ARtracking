e54827d97631592ea3f06ef9f6d2fc56bb4fb5b8
Ivan <vakulko.ivan@gmail.com>
2020-06-13 18:44:19 +0200

    Troche porawiłem

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -139,7 +139,7 @@ public class GameManager : MonoBehaviour
         cm.gameObject.SetActive(false);
 
         cluesFoundCount++;
-        MessageBox.text = "Clues found: " + cluesFoundCount;
+        MessageBox.text = "You found " + cluesFoundCount + "clues";
         //if (cluesFoundCount >= requiredAmountOfClues)
         //{
         //    CurrentGameState = GameState.GS_CLOSE_TO_ANIMAL; //for now it will jump imidiatly to animal close state
